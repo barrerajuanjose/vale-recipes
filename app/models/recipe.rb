be34f01b908c8d9f5c18bb5ad34ddb0478c8808c
Juan Jose Barrera <barrerajuanjose@gmail.com>
2015-03-13 23:24:40 -0300

    added model`s validation

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,10 @@
 class Recipe < ActiveRecord::Base
+	validates :category, presence: true,
+                    length: { minimum: 2 }
+
+    validates :title, presence: true,
+                    length: { minimum: 5 }
+
+    validates :text, presence: true,
+                    length: { minimum: 5 }
 end
